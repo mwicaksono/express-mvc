0e6c5536622122774eda7f4424e175bb80ad386e
MASAN WICAKSONO <mwicaksono99@gmail.com>
2023-02-07 21:58:10 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const express = require('express');
+const session = require('express-session');
+
+const csrf = require('csurf');
+
+const authMiddleware = require('./middlewares/auth-middleware');
+const sessionStore = require('./config/session');
+const db = require('./data/database');
+const authRoutes = require('./routes/auth');
+const blogRoutes = require('./routes/blog');
+
+const mongoDbSessionStore = sessionStore.createSessionStore(session);
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session(sessionStore.createSessionConfig(mongoDbSessionStore)));
+app.use(csrf());
+
+app.use(authMiddleware);
+
+app.use(authRoutes);
+app.use(blogRoutes);
+
+app.use(function (error, req, res, next) {
+  res.render('500');
+})
+
+db.connectToDatabase().then(function () {
+  app.listen(3000);
+});
